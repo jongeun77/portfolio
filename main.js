92bcc413e43e8b6fb6297b80d5adaf6bbeb9b782
jongeun77 <jenn.oh1110@gmail.com>
2021-09-25 19:27:54 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -13,3 +13,17 @@ document.addEventListener('scroll', () =>{
     navbar.classList.remove('navbar--dark');
   }
 })
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click',(event) =>{
+  console.log(event.target.dataset.link); //data-link에 만든 아이디가 나오게 함
+  const target = event.target;//undefined처리위해 아이디가 클릭되면 데이타 링크가 나오게하게끔
+  const link = target.dataset.link; //언디파인일때는 하지말고.
+  if(link ==null){
+    return;
+  }
+  console.log(event.target.dataset.link);
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior: 'smooth'});
+})
