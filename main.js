80dfa35c73b2c1a63c168115d9ff733255a5e83d
jongeun77 <jenn.oh1110@gmail.com>
2021-10-04 23:41:10 +0900

    JS: Navbar menu toggle + project ui tweak

--- main.js
+++ main.js
@@ -12,7 +12,8 @@ document.addEventListener('scroll', () =>{
   }else{
     navbar.classList.remove('navbar--dark');
   }
-})
+});
+
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click',(event) =>{
@@ -22,10 +23,16 @@ navbarMenu.addEventListener('click',(event) =>{
   if(link ==null){
     return;
   }
-  
+  navbarMenu.classList.remove('open');
   console.log(event.target.dataset.link);
   scrollIntoView(link);
 })
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
 
 //Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact ')
